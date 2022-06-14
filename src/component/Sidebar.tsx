d33f618a0b78f481621c4f3c748e94e927c8b2b2
Green <shren0812@gmail.com>
2022-06-14 18:24:01 +0900

    Routing 수정

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -11,26 +11,13 @@ export default function Sidebar() {
     <ul className="bg-amber-200 w-60">
       {dummy.map((item: Dummy, index: number) => {
         return (
-          <li key={index} className={"list-primary"}>
-            <Link to={`/${index}`}>{item.title}</Link>
-          </li>
+          <Link to={`/${index}`}>
+            <li key={index} className={"list-primary"}>
+              {item.title}
+            </li>
+          </Link>
         );
       })}
-      {/*<Link to={}>*/}
-      {/*  <li className="list-primary list-container">Flex Container</li>*/}
-      {/*</Link>*/}
-      {/*<li className="list-primary list-container">display</li>*/}
-      {/*<li className="list-primary list-container">flex-direction</li>*/}
-      {/*<li className="list-primary list-container">flex-wrap</li>*/}
-      {/*<li className="list-primary list-container">justify-content</li>*/}
-      {/*<li className="list-primary list-container">align-items</li>*/}
-      {/*<li className="list-primary list-container">align-content</li>*/}
-      {/*<li className="list-primary list-item">Flex Item</li>*/}
-      {/*<li className="list-primary list-item">order</li>*/}
-      {/*<li className="list-primary list-item">flex-grow</li>*/}
-      {/*<li className="list-primary list-item">flex-basis</li>*/}
-      {/*<li className="list-primary list-item">flex-shrink</li>*/}
-      {/*<li className="list-primary list-item">align-self</li>*/}
     </ul>
   );
 }
