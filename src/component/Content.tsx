a65956a9ff82763d240f5fc5cad8f5f0d2643871
Green <shren0812@gmail.com>
2022-06-14 19:30:04 +0900

    조건부 렌더링 작성

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -4,12 +4,12 @@ import { dummy } from "../static/data";
 export default function Content(): JSX.Element {
   const { id } = useParams();
   if (id === undefined) {
-    return (
-      <div className={"bg-blue-300 w-full flex flex-col items-center"}>
-        <h1 className={"mt-16 mb-32 text-8xl"}>Display</h1>
-        <h1 className={"w-9/12 h-3/6 bg-amber-100"}> contents here</h1>
-      </div>
-    );
+    return <div>404 Error</div>;
   }
-  return <h1 className="bg-blue-300 w-full">Content {dummy[+id].title}</h1>;
+  return (
+    <div className={"bg-blue-300 w-full flex flex-col items-center"}>
+      <h1 className={"mt-16 mb-32 text-8xl"}>{dummy[+id].title}</h1>
+      <h1 className={"w-9/12 h-3/6 bg-amber-100"}> contents here</h1>
+    </div>
+  );
 }
