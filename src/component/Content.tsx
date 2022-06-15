d1bd7fece6770d2251b1e7a06b59aeda0d14f132
Green <shren0812@gmail.com>
2022-06-15 11:12:06 +0900

    test

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -7,9 +7,32 @@ export default function Content(): JSX.Element {
     return <div>404 Error</div>;
   }
   return (
-    <div className={"bg-blue-300 w-full flex flex-col items-center"}>
+    <article className={"bg-blue-300 w-full flex flex-col items-center"}>
       <h1 className={"mt-16 mb-32 text-8xl"}>{dummy[+id].title}</h1>
-      <h1 className={"w-9/12 h-3/6 bg-amber-100"}> contents here</h1>
-    </div>
+      <h1 className={"bg-white w-9/12 h-3/6 border-4"}>
+        <div className={"bg-red-600 m-1 flex"}>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            First
+          </span>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            Second
+          </span>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            Third
+          </span>
+        </div>
+        <div className={"bg-red-600 m-1 flex"}>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            First
+          </span>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            Second
+          </span>
+          <span className={"bg-black text-white m-1 p-10 rounded-md"}>
+            Third
+          </span>
+        </div>
+      </h1>
+    </article>
   );
 }
